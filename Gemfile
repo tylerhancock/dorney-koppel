1c9a74271c416a509659eb6c1097623906f0df5a
Tyler Hancock <tyler.hancock28@gmail.com>
2020-02-04 08:40:47 -0500

    init commit

--- Gemfile
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
-
-git_source(:github) {|repo_name| "https://github.com/dorney-koppel" }
-
-# gem "rails"
-
-gem "jekyll", "~> 4.0"
---
+++ assets/scripts/video.js
@@ -0,0 +1,45 @@
+$( document ).ready(function($) {
+  $('#openButton').click(function() {
+    $('#myVideo')[0].paused ? $('#myVideo')[0].play() : $('#myVideo')[0].pause();
+  });
+});
+
+$( document ).ready(function($) {
+  $('#closeButton').click(function() {
+    $('#myVideo')[0].paused ? $('#myVideo')[0].play() : $('#myVideo')[0].pause();
+  });
+});
+
+$( document ).ready(function($) {
+  $('#myVideo').click(function() {
+    $('#myVideo')[0].paused ? $('#myVideo')[0].play() : $('#myVideo')[0].pause();
+  });
+});
+
+$('#openButton').click(function() {
+  $('#myVideo').toggle('slow', function() {
+    // opens the video player overlay
+  });
+  $('#closeButton').toggle('slow', function() {
+    // displays the clsoe button
+  });
+});
+
+$('#closeButton').click(function() {
+  $('#myVideo').toggle('slow', function() {
+    // closes the video player overlay
+  });
+  $('#closeButton').toggle('slow', function() {
+    // makes close button disappear
+  });
+});
+
+$('#myVideo').click(function() {
+  $('#myVideo').toggle('slow', function() {
+    // closes the video player overlay
+  });
+  $('#closeButton').toggle('slow', function() {
+    // displays the clsoe button
+  });
+});
+      
